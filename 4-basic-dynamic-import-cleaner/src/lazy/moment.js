75f89b924029bfe58473913759f788238b3bf8d8
Andy <andymeps@yahoo.com>
2019-04-05 22:02:56 +0100

    Remove wrong comments from moment.js

--- 4-basic-dynamic-import-cleaner/src/lazy/moment.js
+++ 4-basic-dynamic-import-cleaner/src/lazy/moment.js
@@ -2,12 +2,4 @@ const getMoment = (callback) => {
   import(/* webpackChunkName: "moment" */ 'moment').then(callback);
 };
 
-// // With async/await:
-
-//const getMoment = async (callback) => {
-//  const moment = await import(/* webpackChunkName: "moment" */ 'moment');
-//  callback(moment);
-//};
-
-
 export default getMoment;
--- 5-how-about-lodash/src/lazy/moment.js
+++ 5-how-about-lodash/src/lazy/moment.js
@@ -2,11 +2,4 @@ const getMoment = (callback) => {
   return import(/* webpackChunkName: "moment" */ 'moment');
 };
 
-// // With async/await:
-
-//const getMoment = async () => {
-//  return await import(/* webpackChunkName: "moment" */ 'moment');
-//};
-
-
 export default getMoment;
